071079d546f055ab02e1fb2fe9deee655d164cae
Lucas Paiva <lpaiva234@gmail.com>
2026-01-08 10:27:16 -0300

    export Ship class

--- Ship.js
+++ Ship.js
@@ -1,12 +1,10 @@
-class Ship {
+export class Ship {
     constructor(length) {
         this.length = length;
-        this.sunk = false;
         this.hits = 0;
     }
     hit() {
         this.hits++;
-        //prevent hit if already sunk?
     }
     isSunk() {
         return this.hits >= this.length;
