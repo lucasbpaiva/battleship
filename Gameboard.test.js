b8dbf18202a501b7382c6298fe6a187da2150d4b
Lucas Paiva <lpaiva234@gmail.com>
2026-01-09 18:05:07 -0300

    Write tests for areAllShipsSunk method

--- Gameboard.test.js
+++ Gameboard.test.js
@@ -1,21 +1,36 @@
 import { Gameboard } from "./Gameboard.js";
 
+beforeEach(() => {
+    let gameboard = new Gameboard();
+});
+
 test("Ships can be placed horizontally", () => {
-    const gameboard = new Gameboard();
     gameboard.placeShip(3, 2, 5, 0);
-    console.log(gameboard.board);
     expect(gameboard.board[5][2][0]).toBe(1);
     expect(gameboard.board[5][3][0]).toBe(1);
     expect(gameboard.board[5][4][0]).toBe(1);
 });
 
 test("Ships can be placed vertically", () => {
-    const gameboard = new Gameboard();
     gameboard.placeShip(5, 7, 2, 1);
-    console.log(gameboard.board);
     expect(gameboard.board[2][7][0]).toBe(1);
     expect(gameboard.board[3][7][0]).toBe(1);
     expect(gameboard.board[4][7][0]).toBe(1);
     expect(gameboard.board[5][7][0]).toBe(1);
     expect(gameboard.board[6][7][0]).toBe(1);
+});
+
+test("Gameboard reports that all ships are sunk", () => {
+    gameboard.placeShip(2, 5, 1, 0);
+    gameboard.receiveAttack(5, 1);
+    gameboard.receiveAttack(6, 1);
+    expect(gameboard.areAllShipsSunk()).toBe(true);
+});
+
+test("Gameboard reports false if not all ships are sunk", () => {
+    gameboard.placeShip(2, 1, 5, 0);
+    gameboard.placeShip(3, 5, 4, 1);
+    gameboard.receiveAttack(1, 5);
+    gameboard.receiveAttack(2, 5);
+    expect(gameboard.areAllShipsSunk()).toBe(false);
 });
