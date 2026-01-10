96240f98e74155d72d512c7669d4c3b6dc71bf64
Lucas Paiva <lpaiva234@gmail.com>
2026-01-09 22:27:14 -0300

    Implement areAllShipsSunk method

--- Gameboard.js
+++ Gameboard.js
@@ -4,9 +4,11 @@ export class Gameboard {
     constructor() {
         //board will be 10x10 array where each element will be a 2-element array [0 for water or 1 for ship, a boolean indicating whether the coordinate has already been fired at]
         this.board = Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => [0, false, null]));
+        this.ships = [];
     }
 
     placeShip(length, startPosX, startPosY, orientation, ship = new Ship(length)) {
+        this.ships.push(ship);
         if (orientation === 0) { //horizontal
             for (let i = 0; i < length; i++) {
                 this.board[startPosY][startPosX + i] = [1, false, ship];
@@ -22,12 +24,16 @@ export class Gameboard {
         //if position has not been fired at yet
         if (this.board[y][x][1] === false) {
             this.board[y][x][1] = true;
-            //determines whether or not a ship was hit
+            //determines whether or not a ship was hit 
             if (this.board[y][x][0] === 1) {
                 //send hit to the ship if it was hit
                 let ship = this.board[y][x][2];
                 ship.hit();
             }
         }
+    }
+
+    areAllShipsSunk() {
+        return this.ships.every((ship) => ship.isSunk());
     }
 }
