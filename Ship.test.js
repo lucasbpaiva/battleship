3d99443ac36ff0889ec71daf94028262aa8cffa5
Lucas Paiva <lpaiva234@gmail.com>
2026-01-13 12:00:42 -0300

    Move files into src/

--- Ship.test.js
+++
@@ -1,9 +0,0 @@
-import { Ship } from "./Ship.js";
-
-test("Ship sinks after getting enough hits", () => {
-    const ship = new Ship(3);
-    ship.hit();
-    ship.hit();
-    ship.hit();
-    expect(ship.isSunk()).toBe(true);
-});
--- main.js
+++
@@ -1,8 +0,0 @@
-const battleshipGrid = document.querySelectorAll(".battleship-grid");
-
-battleshipGrid.forEach((currentGrid) => {
-    for (let i = 0; i < 100; i ++) {
-        const square = document.createElement("div");
-        currentGrid.appendChild(square);
-    }
-});
