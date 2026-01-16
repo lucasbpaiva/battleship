f2c69b4b26ab8c5aa01a1c67007be73cc1f9e82a
Lucas Paiva <lpaiva234@gmail.com>
2026-01-16 19:52:35 -0300

    Refactor guessing logic for computer

--- src/Player.js
+++ src/Player.js
@@ -16,9 +16,29 @@ export class Player {
 export class ComputerPlayer extends Player {
     constructor() {
         super("Computer");
+        this.targetQueue = []; // squares to attack next after a hit
     }
 
-    randomAttack(enemyGameboard) {
+    makeMove(enemyGameboard) {
+        let x, y;
+
+        // target mode (pick from queue)
+        if (this.targetQueue.length > 0) {
+            // parentheses needed for this destructuring assignment
+            ({ x, y } = this.targetQueue.shift());
+        } else { // hunt mode (random attack)
+            ({ x, y } = this.#getRandomCoords(enemyGameboard));
+        }
+
+        enemyGameboard.receiveAttack(x, y);
+
+        // if it was a hit, add neighbours to the queue
+        if (enemyGameboard.board[y][x][0] === 1) {
+            this.#addNeighboursToQueue(x, y, enemyGameboard);
+        }
+    }
+
+    #getRandomCoords(enemyGameboard) {
         let validMove = false;
         let x, y;
 
@@ -31,6 +51,20 @@ export class ComputerPlayer extends Player {
             }
         }
 
-        enemyGameboard.receiveAttack(x, y);
+        return { x, y };
+    }
+
+    #addNeighboursToQueue(x, y, enemyGameboard) {
+        const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]; // up, down, right, left
+        directions.forEach(([dx, dy]) => {
+            const nx = x + dx;
+            const ny = y + dy;
+            const withinBounds = nx >= 0 && nx < 10 && ny >= 0 && ny < 10;
+            const firedAt = enemyGameboard.board[ny][nx][1];
+            const duplicate = this.targetQueue.some(t => t.x === nx && t.y === ny);
+            if (withinBounds && !firedAt && !duplicate) {
+                this.targetQueue.push({ x: nx, y: ny });
+            }
+        });
     }
 }
